f2bb185eb4114fbd9732e0838e10c2d60c455061
Jonathan Faivre <ext.Jonathan.Faivre@lotsys.com>
2017-04-07 18:03:04 +0200

    add text on intro

--- src/element/Intro.ts
+++ src/element/Intro.ts
@@ -4,10 +4,12 @@ import Panel from '../ui/Panel';
 import BtnPlay from './btn/BtnPlay';
 import Scale9Grid from '../ui/Scale9Grid';
 import { Dock } from '../ui/Layout';
+import TextField from '../ui/TextField';
 
 export default class Intro extends Panel {
 
   playButton: BtnPlay;
+  content: TextField;
 
   constructor() {
 
@@ -15,9 +17,27 @@ export default class Intro extends Panel {
 
     let defaultBgTextureBtn: Texture = Texture.fromImage('button_blue');
 
+    this.content = new TextField('<h1>This game is a poc</h1><p>This is a textfield, work with svg font TTF OTF or WOFF</p>', {
+      default: {
+        fontName: 'AcmeFont',
+        fill: 'grey',
+        shadowColor: 'white',
+        shadowBlur: 10,
+        shadowOffsetX: 5
+      },
+      h1: {
+        fill: 'black'
+      },
+    });
+    this.content.wordWrap = true;
+    this.content.width = this.width;
+    this.content.dock = Dock.CENTER | Dock.TOP;
+    this.content.dockY = 20;
+    this.addChild(this.content);
+
     this.playButton = new BtnPlay();
     this.playButton.dock = Dock.BOTTOM | Dock.CENTER;
-    this.playButton.dockY = -20;
+    this.playButton.dockY = 20;
     this.addChild(this.playButton);
 
     this.playButton.on('click', this.click.bind(this));
